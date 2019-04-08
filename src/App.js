0cac42e6a8648785169b7b55f0b5b0973c3f4d06
Marcel Radix <mail@marcelradix.nl>
2019-04-08 12:46:43 +0000

    period push

--- src/App.js
+++ src/App.js
@@ -1,71 +1,23 @@
 import React, { Component } from 'react';
-import {
-  Route,
-  Link,
-  Redirect,
-  BrowserRouter as Router,
-} from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import firebase from './components/firebase/firebase'
 
 import uw_auth from './components/uw_auth'
 import uw_status from './components/uw_status'
 
 
+
 import './App.css';
-import './styles/css/styles.css';
-
-const Public = () => (
-  <div> This is a public page </div>
-);
-
-
-
-const Private = () => (
-  <div> This is a private page </div>
-);
-
-const Login = () => (
-  <div> Login Page <button>login</button> </div>
-);
-
-
-
-const AuthService = {
-  isAuthenticated: false,
-  authenticate(cb) {
-    this.isAuthenticated = true
-    setTimeout(cb, 100)
-  },
-  logout(cb) {
-    this.isAuthenticated = false
-    setTimeout(cb, 100)
-  }
-};
-
-
-
-const SecretRoute = ({ component: Component, ...rest }) => (
-  <Route {...rest} render={(props) => (
-    AuthService.isAuthenticated === true
-      ? <Component {...props} />
-      : <Redirect to={{
-        pathname: '/login',
-        state: { from: props.location }
-      }} />
-  )} />
-);
+import './styles/css/styles.css'
 
 
 class App extends Component {
   render() {
     return (
-      <div style={{ width: 1000, margin: '0 auto' }}>
+      <div>
         <ul>
-          <li><Link to='/public'> Public </Link></li>
-          <li><Link to='/private'> Private </Link></li>
+          <li><Link to="/authenticate">Authenticate</Link></li>
         </ul>
-        <Route path='/public' component={Public} />
-        <SecretRoute path='/private' component={Private} />
         <Route exact path="/authenticate" component={uw_auth} />
         <Route path="/authenticate/status" component={uw_status} />
       </div>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import App from './App';
 
-
 import config from './components/firebase/firebase';
 import * as firebase from 'firebase'
 import { FirebaseContext } from './components/firebase';
