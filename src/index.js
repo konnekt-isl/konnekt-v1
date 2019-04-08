47ee28404db97b9efc792e5e6fe820932013c243
Marcel Radix <mail@marcelradix.nl>
2019-04-08 11:04:48 +0000

    Adding secured router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import App from './App';
 
+
 import config from './components/firebase/firebase';
 import * as firebase from 'firebase'
 import { FirebaseContext } from './components/firebase';
