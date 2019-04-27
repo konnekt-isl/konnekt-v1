0bddc09c7fc03faa8061763ea684df38eabfb18f
Kamilla Henriau <kamillahenriau@Kamilla-macci.local>
2019-04-27 15:34:59 +0000

    edit landingpage styles

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -13,25 +13,25 @@ const Landing = () => (
     </div>
     <div className="section">
       <a id="lausnir" />
-      <div className="summary-wrapper">
+      <div className="summary-wrapper m-h">
         <h3>Hvað er?</h3>
         <h4>Konnekt</h4>
         <p>Með Konnekt opna fyrirtæki fyrir örugga samskiptaleið á milli þeirra og viðskiptavinarins. Þar er hægt að afhenda upplýsingar og gögn á öruggan hátt á rafrænum vettvangi.</p>
         <p>Konnekt kemur til móts við þarfir fólks um að geta fengið einstaklingsmiðaða þjónustu og ráðgjöf á netinu. Það vill geta spurt strax og fengið skjót svör óháð staðsetningu. Ferlið er hratt og auðvelt í notkun. </p>
       </div>
-      <div className="summary-decoration">
+      <div className="summary-decoration m-v">
 
       </div>
     </div>
 
     <div className="section">
-      <div className="summary-decoration">
-        <video width="320" height="240" controls>
+      <div className="summary-decoration m-h">
+        <video width="600" height="320" controls>
           {/* <source src="movie.mp4" type="video/mp4">
             <source src="movie.ogg" type="video/ogg"> */}
         </video>
       </div>
-      <div className="summary-wrapper">
+      <div className="summary-wrapper m-v">
         <h3>Hvernig virkar það?</h3>
         <h4>Virkar fyrir allt og alla</h4>
         <p>Konnekt er hægt að bæta við hvaða stafræna vettvang sem er, hvort sem það sé núverandi grunnkerfi eða samfélagsmiðla tól. Hér til hliðar sérðu dæmi um notkun. Möguleikarnir eru endalausir. </p>
@@ -40,13 +40,13 @@ const Landing = () => (
 
     <div className="section">
       <a id="demo" />
-      <div className="summary-wrapper">
+      <div className="summary-wrapper m-h">
         <h3>Viltu prófa?</h3>
         <h4>Sjáðu snilldina</h4>
         <p>Segðu okkur hvort þú sért fyrirtæki eða einstaklingur og við leiðum þig áfram. </p>
         {/* <SVGIcon className="arrow" name="arrow" fill="#000" width={431} height={8}/> */}
       </div>
-      <div className="summary-decoration">
+      <div className="summary-decoration m-v">
         <button className="big-btn blue">Einstaklingur</button>
         <button className="big-btn green">Fyrirtæki</button>
       </div>
@@ -55,10 +55,10 @@ const Landing = () => (
     <div className="section okkar-syn-section">
 
       {/* <img className="okkar-syn" src={okkarsyn} alt="" /> */}
-      <div className="summary-decoration okkar-syn-container">
+      <div className="summary-decoration okkar-syn-container m-h">
         {/* <img className="okkar-syn" src={okkarsyn} /> */}
       </div>
-      <div className="summary-wrapper">
+      <div className="summary-wrapper m-v">
         <a id="okkar-syn" />
         <h3>Framtíðin</h3>
         <h4>Okkar sýn</h4>
