c468f58342dcae60a0035a2a813025626a72c043
Marcel Radix <mail@marcelradix.nl>
2019-05-02 08:17:13 +0000

    Fixin Sign Out button

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -4,13 +4,12 @@ import { withFirebase } from '../Firebase';
 
 const SignOutButton = ({ firebase }) => (
   <div>
-  <button type="button" onClick={firebase.doSignOut}>
-  Signout
-  </button>
-  <SVGIcon name="logout" width={24} className="logout-icon"/>
+    <button type="button" onClick={firebase.doSignOut}>
+      <SVGIcon name="logout" width={24} className="logout-icon" />
+    </button>
   </div>
 
- 
+
 );
 
 export default withFirebase(SignOutButton);
