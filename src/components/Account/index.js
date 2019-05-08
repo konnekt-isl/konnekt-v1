0b11d3ec66f4e6d4eb3d989695ed0502b025d225
EllenAsta <ellenastavanbeek@gmail.com>
2019-05-08 15:54:09 +0000

    Styled Account/Settings page

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { compose } from 'recompose';
-
+import settings from '../img/settings.svg';
 import {
   AuthUserContext,
   withAuthorization,
@@ -32,11 +32,20 @@ const SIGN_IN_METHODS = [
 const AccountPage = () => (
   <AuthUserContext.Consumer>
     {authUser => (
-      <div>
-        <h1>Account: {authUser.email}</h1>
-        <PasswordForgetForm />
-        <PasswordChangeForm />
-        {/* <LoginManagement authUser={authUser} /> */}
+      <div className="account-page">
+        <div className="account-wrapper">
+        <div className="account-container">
+          <img className="account-img" src={settings} />
+          <h1>Hæ {authUser.username}</h1>
+          <p>Hér getur þú breytt um lykilorð. 
+            Sláðu inn netfang og þú færð link sendan í tölvupósti.</p>
+            <PasswordForgetForm />
+            {/* <PasswordChangeForm /> */}
+            {/* <LoginManagement authUser={authUser} /> */}
+        </div>
+      
+        </div>
+     
       </div>
     )}
   </AuthUserContext.Consumer>
--- src/components/PasswordForget/index.js
+++ src/components/PasswordForget/index.js
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
-
+import SVGIcon from "../img/SVGIcon";
+import SignOutButton from '../SignOut';
 const PasswordForgetPage = () => (
   <div>
     <h1>PasswordForget</h1>
@@ -48,16 +49,17 @@ class PasswordForgetFormBase extends Component {
     const isInvalid = email === '';
 
     return (
-      <form onSubmit={this.onSubmit}>
-        <input
+      <form className="account-form" onSubmit={this.onSubmit}>
+      <label for="email">Netfang:</label>
+        <input className="account-input"
           name="email"
           value={this.state.email}
           onChange={this.onChange}
           type="text"
           placeholder="Email Address"
         />
-        <button disabled={isInvalid} type="submit">
-          Reset My Password
+        <button className="account-btn" disabled={isInvalid} type="submit">
+          Senda
         </button>
 
         {error && <p>{error.message}</p>}
