0d01dc85215f0ca39724c898dcd12b76a3f2f8ff
Elsa Bjarnadóttir <elsabj@gmail.com>
2019-05-09 14:48:24 +0000

    landing page, mobile + desktop, konnekt people for landinga page

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -90,7 +90,7 @@ const Landing = () => (
 
       <div class="list-container m-h">
         <a id="samband" />
-        <h2>ER ÞITT FYRIRTÆKI TILBÚIÐ TIL ÞESS AÐ ...</h2>
+        <h2>ER ÞITT FYRIRTÆKI TILBÚIÐ TIL ÞESS AÐ...</h2>
         <ul>
           <div className="list-item">
             <div className="dot"></div><li>Ræða um framtíðina ?</li>
@@ -104,7 +104,7 @@ const Landing = () => (
         </ul>
       </div>
       <div class="contact-link-container m-v">
-        <p>Sendu okkur línu á ...</p>
+        <p class="senduokkurlinu">Sendu okkur línu á</p>
         <a className="contact-link">hello@konnekt.is</a>
       </div>
 
